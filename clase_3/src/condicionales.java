2d810a51452856e6e10ab5244af17afa22b75452
Cristhian Leon <cristhiancleon@gmail.com>
2026-06-27 14:24:58 -0500

    clase 3

---
+++ clase_3/src/condicionales.java
@@ -0,0 +1,29 @@
+public class condicionales {
+    public static void main(String[] args) {
+        double cuenta = 150000;
+
+        // if (cuenta > 100000) {
+        // System.out.println("Aplica descuento");
+
+        // }
+        // else {
+
+        // }
+
+        if (cuenta >= 200000) {
+
+            System.out.println("Descuento VIP (15%)");
+
+        } else if (cuenta > 100000) {
+
+            System.out.println("Descuento normal (10%)");
+
+        } else {
+
+            System.out.println("Sin descuento");
+
+        }
+
+    }
+
+}
